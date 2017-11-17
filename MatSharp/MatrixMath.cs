ca4f745c70927b8a5895a9ef526378675e921be0
Louise Abrahamsson Kwetczer <louisekw@kth.se>
2017-11-18 00:46:04 +0100

    Added exp

--- MatSharp/MatrixMath.cs
+++ MatSharp/MatrixMath.cs
@@ -15,7 +15,29 @@ namespace MatSharp {
             => Round(matrix,0);
 
         public static Matrix Pow(Matrix x, int y){
-            
+            Matrix ret = x;
+            for(int i = 0; i < y; i++)
+                ret *= x;
+            return ret;
+        }
+
+        /// <summary>
+        ///     Returns an approximation of e^A based on Taylor expansion of degree n
+        public static Matrix Exp(Matrix A, int n){
+            if(A.Rows != A.Columns)
+                throw new ArgumentException("Matrix dimensions do not agree");
+
+            Matrix curr = Matrix.Identity(A.Columns);
+            Matrix ret = curr;
+
+            int fac = 1;
+            for(int i = 1; i < n; i++){
+                fac *= i;
+                curr *= A;
+                ret += 1 / fac * curr;
+            }
+
+            return ret;
         }
     }
 }
